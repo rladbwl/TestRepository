753a61c20124b3bf8f20baf7a32f9757ddfdac4f
yujin <kimyujin2488@naver.com>
2026-01-20 15:47:05 +0900

    i 첫 저장소 설장~

---
+++ maiin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Log.h"
+using namespace std;
+
+//void Log(const char* message);
+
+//
+// Ctrl + 방향키 좌우
+// Home + End + (shift)
+int main() {
+
+	Log("Hello World");
+	//std::cout << "test\n";
+
+	cin.get();
+}
